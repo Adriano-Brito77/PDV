ff5eaaec22a965afdc20328966c7c560d473820c
Adriano-Brito77 <102468294+Adriano-Brito77@users.noreply.github.com>
2024-11-01 21:06:30 -0300

    getUserByToken

--- backend/helpers/check-Token.js
+++ backend/helpers/check-Token.js
@@ -2,14 +2,11 @@ const jwt = require("jsonwebtoken");
 const getToken = require("./get-token");
 
 const checkToken = (req, res, next) => {
-  console.log(req.headers);
-
   if (!req.headers.authorization) {
     return res.status(401).json({ message: "Acesso Negado" });
   }
 
   const token = getToken(req);
-  console.log(token);
 
   if (!token) {
     return res.status(401).json({ message: "Acesso negado" });
